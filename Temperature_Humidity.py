266fa8e8581361a6ae4e8ad5cb41b63bad2bcc65
rezafarshidi <reza.farshidi@gmail.com>
2020-07-19 18:48:24 -0300

    corrected for python 3

--- Temperature_Humidity.py
+++ Temperature_Humidity.py
@@ -14,7 +14,7 @@ while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
 # sudo python Temperature_Humidity.py
 
